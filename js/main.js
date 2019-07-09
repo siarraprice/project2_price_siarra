d348def984664b9fcb3d1d2ff2ea34f33e5a6e6b
Siarra Price <siarraprice@ufl.edu>
2019-07-08 23:12:25 -0500

    added content about each venue, moved the image gallery to venye page

--- js/main.js
+++ js/main.js
@@ -2,4 +2,12 @@ $("#banner").backstretch("images/RoseGold.jpg");
 
 $("#banner2").backstretch("images/RoseGold.jpg");
 
+$("#banner3").backstretch("images/RoseGold.jpg");
+
+$("#banner4").backstretch("images/westhall.jpg");
+
+$("#banner5").backstretch("images/lightbox9.jpg");
+
+$("#banner6").backstretch("images/stainedglass.jpg");
+
 var lightbox = $('.gallery a').simpleLightbox(options);
